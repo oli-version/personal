0ec3ae9833ad89e55896c51c6df6acd1c30ca53c
Oliver <71002514+zlbyapi@users.noreply.github.com>
2021-11-09 20:19:19 +0200

    update

--- src/Hobies.svelte
+++ src/Hobies.svelte
@@ -1,12 +1,12 @@
 <script>
-
+    let target = '_blank';
 </script>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">
 
 <main>
     <h1>My Hobbies</h1>
-    <p>I like playing games on such as <b>Minecraft</b> and <b>Stardew Valley</b>. You can see me playing sometimes on my <span class="tag"><a href="https://youtube.com"><i class="fa fa-twitch"></i></a></span></p>
+    <p>I like playing games on such as <b>Minecraft</b> and <b>Stardew Valley</b>. You can see me playing sometimes on my <span class="tag"><a {target} href="https://twitch.tv/zlbylive"><i class="fa fa-twitch"></i> Twitch</a></span></p>
 </main>
 
 <style>
